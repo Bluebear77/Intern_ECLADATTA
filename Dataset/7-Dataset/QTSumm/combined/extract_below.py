5c3b01aaa989235e2c69cd30287e53eb28a296d2
Bluebear77 <119409649+Bluebear77@users.noreply.github.com>
2024-06-07 13:42:13 +0000

    x

--- Dataset/7-Dataset/QTSumm/combined/extract_below.py
+++ Dataset/7-Dataset/QTSumm/combined/extract_below.py
@@ -9,13 +9,20 @@ df_below_threshold = pd.read_csv(csv_file)
 # Get the list of table_ids from the CSV file
 table_ids = df_below_threshold['table_id'].tolist()
 
-# Load the merged_qtsumm.json file
-json_file = '../merged_qtsumm.json'
-with open(json_file, 'r') as f:
-    data = json.load(f)
+# List of JSON files in the parent directory
+json_files = ["../qtsumm_dev.json", "../qtsumm_test.json", "../qtsumm_train.json"]
+
+# Initialize a list to hold all data from the JSON files
+all_data = []
+
+# Iterate through the JSON files and load the data
+for json_file in json_files:
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        all_data.extend(data)
 
 # Filter the instances based on the table_id
-filtered_instances = [instance for instance in data if instance['table']['table_id'] in table_ids]
+filtered_instances = [instance for instance in all_data if instance['table']['table_id'] in table_ids]
 
 # Save the filtered instances to a new JSON file
 output_file = 'filtered_qtsumm.json'
