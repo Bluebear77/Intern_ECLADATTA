65a6ec5aea2339cedf574aa8756e6f5da2f73de6
Bluebear77 <119409649+Bluebear77@users.noreply.github.com>
2024-07-18 12:38:24 +0000

    x

--- Dataset/ReasTAP/ReasTAP-main/synthetic_tableqa_generation/generate_synthetic_tableqa.py
+++ Dataset/ReasTAP/ReasTAP-main/synthetic_tableqa_generation/generate_synthetic_tableqa.py
@@ -86,8 +86,8 @@ def log_error(message):
 
 def main():
     template_dict = json.load(open("question_template.json"))
-    #table_data_dir = "../../../subcorpus-100-business-telco-celebrity/business-100/P5"
-    table_data_dir = "../../../100-female-celebrities/v2-100/P6"
+    table_data_dir = "../../../subcorpus-100-business-telco-celebrity/telco-100/P5"
+    #table_data_dir = "../../../100-female-celebrities/v2-100/P6"
     
     files = [f for f in os.listdir(table_data_dir) if f.endswith(".json")]
     output_dir = "output"
